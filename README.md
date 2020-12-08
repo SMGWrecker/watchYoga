5984f96551a3f4c8f939cff90d2ece94faf566a9
Test <Test>
2020-12-08 13:15:39 +0300

    Yoga

---
+++ README.md
@@ -0,0 +1,1 @@
+Yoga Watch OS
---
+++ watchYoga WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,48 @@
+//
+//  InterfaceController.swift
+//  watchYoga WatchKit Extension
+//
+//  Created by user on 08/12/2020.
+//
+
+import WatchKit
+import Foundation
+import WatchConnectivity
+
+
+class InterfaceController: WKInterfaceController, WCSessionDelegate {
+    
+    var wcSession: WCSession!
+    
+    @IBOutlet weak var name: WKInterfaceLabel!
+    @IBOutlet weak var timer: WKInterfaceTimer!
+    @IBOutlet weak var image: WKInterfaceImage!
+    
+    
+    func session(_ session: WCSession, activationDidCompleteWith activationState: WCSessionActivationState, error: Error?) {
+        
+    }
+    
+    func session(_ session: WCSession, didReceiveApplicationContext applicationContext: [String : Any]) {
+        let message = applicationContext["name"] as? String
+        let time = applicationContext["time"] as? Double
+        name.setText(message)
+        timer.setDate(NSDate(timeIntervalSinceNow: time!) as Date)
+        timer.start()
+    }
+
+    override func awake(withContext context: Any?) {
+        wcSession = WCSession.default
+        wcSession.delegate = self
+        wcSession.activate()
+    }
+    
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+    }
+    
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+    }
+
+}
